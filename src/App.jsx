ad23abbe8a0fc2b05638c3eddcb037f60dfd715f
javiluna00 <javilunac00@gmail.com>
2024-10-22 18:03:49 +0200

    Cambios en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import Home from './pages/home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainLayout from './layout/MainLayout'
 import Carta from './pages/Carta'
-import AdminLogin from './pages/AdminLogin'
 import { Toaster } from 'sonner'
-import AdminLayout from './layout/AdminLayout'
-import EditarCarta from './pages/EditarCarta'
-import NuevoPlato from './pages/NuevoPlato'
+
 
 
 function App() {
@@ -24,11 +21,6 @@ function App() {
           <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />} />
             <Route path='carta' element={<Carta />} />
-          </Route>
-          <Route path='/admin' element={<AdminLayout />} >
-            <Route index element={<AdminLogin />} />
-            <Route path='carta' element={<EditarCarta />} />
-            <Route path='carta/nuevo' element={<NuevoPlato />} />
           </Route>
         </Routes>
       </BrowserRouter>
